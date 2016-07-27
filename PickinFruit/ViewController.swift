16fbe39d7ee09fd6a0201734f02d884375efd9b4
dochonglo <ismaelbarry777@hotmail.com>
2016-07-27 09:58:06 -0400

    Pickin Fruit Lab is Complete.

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,24 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var fruitPicker: UIPickerView!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
+        // Set the delegate to self so we can add the required methods:
+        self.fruitPicker.delegate = self
+        
+        // Set the dataSource to self so we can add the required methods:
+        self.fruitPicker.dataSource = self
+        
+        self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
         
     }
     
     @IBAction func spinButtonTapped(sender: UIButton) {
         
+        let randomNumber1 = Int(arc4random_uniform(UInt32(self.fruitsArray.count)))
+        let randomNumber2 = Int(arc4random_uniform(UInt32(self.fruitsArray.count)))
+        let randomNumber3 = Int(arc4random_uniform(UInt32(self.fruitsArray.count)))
         
+        self.fruitPicker.selectRow(randomNumber1, inComponent: 0, animated: true)
+        self.fruitPicker.selectRow(randomNumber2, inComponent: 1, animated: true)
+        self.fruitPicker.selectRow(randomNumber3, inComponent: 2, animated: true)
+
+        
+        let firstIndex = self.fruitPicker.selectedRowInComponent(0)
+        let secondIndex = self.fruitPicker.selectedRowInComponent(1)
+        let thirdIndex = self.fruitPicker.selectedRowInComponent(2)
+        
+        if (firstIndex == secondIndex) && (secondIndex == thirdIndex) {
+            
+            self.resultLabel.text = "WINNER!"
+            
+        } else {
+            
+            self.resultLabel.text = "TRY AGAIN"
+        }
+    }
+    
+    // Implement the pickerView(_:titleForRow:forComponent:) method to set the title from the strings within the array:
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return self.fruitsArray[row % self.fruitsArray.count]
+    }
+    
+    // Necessary data source delegate methods to provide data to the picker view.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
+        // We have only one component for the picker view so we return a literal 1.
+        return 3
+    }
+    
+    // Necessary data source delegate methods to provide data to the picker view.
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return 1000
+        // Using .count we get the number of rows from the data.
+        //return self.fruitsArray.count
     }
     
 }
@@ -48,6 +96,3 @@ extension ViewController {
     }
     
 }
-
-
-
